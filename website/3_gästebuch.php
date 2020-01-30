ce875aa413937a7c80a0223fe946ad0f88dd34f1
Jan Grossenbacher <jan.louis.gro@gmail.com>
2020-01-30 15:15:01 +0100

    30.01.2020

--- website/3_gästebuch.php
+++ website/3_gästebuch.php
@@ -1,5 +1,5 @@
 <?php
-    include_once 'includes/dbverbindung.php'
+    include_once 'includes/GuestbookAccess.php'
  ?>
 
 <!DOCTYPE html>
@@ -14,7 +14,6 @@
   <body>
 
 
-
         <ul>
           <li><a href="0_index.html">Home</a></li>
           <li><a href="1_geschichte.html">Geschichte</a></li>
@@ -24,36 +23,11 @@
 
         <div>
         <h1>Das Gästebuch</h1>
-<?php
-
-
-        // Get all entries in the guestbook
-        $table = $guestbook->getEntries();
-
-        if ($table) { // Check if there are enrtries
-            echo "\nThe guestbook contains:\n";
-            foreach ($table as $row) {
-                // Output each element
-                $index = $row["Index"];
-                $name = $row["Name"];
-                $date = $row["Date"];
-                $email = $row["eMail"];
-                $comment = $row["Comment"];
-
-                echo "Index: $index, ";
-                echo "Name: $name, ";
-                echo "Date: $date, ";
-                echo "eMail: $email, ";
-                echo "Comment: $comment\n";
-            }
-?>
 
 
         <h2>Hinterlassen Sie der Community eine Nachricht</h2>
           <p>
 
-              <label for="fname">Vorname: </label>
-              <input id="fname" type="text" value="" maxlength="20" size="15" name="New_Name">
               <label for="lname">Nachname: </label>
               <input id="lname" type="text" value="" maxlength="40" size="30" name="New_Name"><br>
 
